8302321f32155d12a83d14c7c603d7ab3cd22cb8
Jishnu-Nandhiath <jishnun789@gmail.com>
2022-02-08 21:40:05 +0530

    Adding starter cpp file for CP competitions

---
+++ Basics/My Starter File.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+void solve()
+{
+
+
+}
+
+
+int32_t main()
+{
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    clock_t start = clock();
+
+    int t = 1;
+    // cin>>t;
+    while(t--) solve();
+
+    cerr << "Run Time : " << ((double)(clock() - start) / CLOCKS_PER_SEC) << " ";
+
+
+    return 0;
+}
